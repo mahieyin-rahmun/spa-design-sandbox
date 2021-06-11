c1747a3d765bda74e2a53dcc0e5a76ba87d80913
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2021-06-11 18:27:38 +0600

    (client) init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout/Layout";
+import { globalLightTheme } from "../styles/globals.styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={globalLightTheme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
