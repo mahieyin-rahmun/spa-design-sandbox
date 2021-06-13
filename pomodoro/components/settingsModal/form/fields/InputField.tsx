a345c13ad8ced076e3c3c91f7e79bf53d82fe234
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2021-06-13 19:51:32 +0600

    (pomodoro) formik fields for input

---
+++ pomodoro/components/settingsModal/form/fields/InputField.tsx
@@ -0,0 +1,65 @@
+import {
+  FormControl,
+  FormLabel,
+  Grid,
+  OutlinedInput,
+  OutlinedInputProps,
+} from "@material-ui/core";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+
+type TInputFieldProps = {
+  name: string;
+  label: string;
+} & OutlinedInputProps &
+  InputHTMLAttributes<HTMLInputElement>;
+
+const InputField: React.FC<TInputFieldProps> = ({
+  children,
+  color: _,
+  ...props
+}) => {
+  const [field, { error }] = useField({
+    name: props.name,
+    type: props.type,
+    validate: (value) => {
+      let message: string | undefined = undefined;
+
+      if (props.type === "number" && (props.min || props.max)) {
+        if (parseFloat(value) > parseFloat(props.max as string)) {
+          message = `The value of ${props.label} cannot be more than ${props.max}`;
+        }
+
+        if (parseFloat(value) < parseFloat(props.min as string)) {
+          message = `The value of ${props.label} cannot be less than ${props.min}`;
+        }
+      }
+      return message;
+    },
+  });
+
+  return (
+    <FormControl error={Boolean(error)}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={4}>
+          <FormLabel htmlFor={props.name}>{props.label}</FormLabel>
+        </Grid>
+        <Grid item xs={8}>
+          <OutlinedInput
+            notched={false}
+            id={props.name}
+            {...field}
+            {...props}
+            inputProps={{
+              min: props.min,
+              max: props.max,
+              step: props.step,
+            }}
+          />
+        </Grid>
+      </Grid>
+    </FormControl>
+  );
+};
+
+export default InputField;
---
+++ pomodoro/components/settingsModal/form/fields/RadioButtonInputField.tsx
@@ -0,0 +1,103 @@
+import {
+  createStyles,
+  Radio,
+  Theme,
+  makeStyles,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  RadioProps,
+  Typography,
+} from "@material-ui/core";
+import clsx from "clsx";
+import { useField } from "formik";
+import React from "react";
+
+const useStyles = (color: string) =>
+  makeStyles((theme: Theme) => {
+    return createStyles({
+      root: {
+        "&:hover": {
+          backgroundColor: "transparent",
+        },
+      },
+      icon: {
+        borderRadius: "50%",
+        width: 40,
+        height: 40,
+        boxShadow:
+          "inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)",
+        backgroundColor: color,
+        backgroundImage:
+          "linear-gradient(0deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
+        "$root.Mui-focusVisible &": {
+          outlineOffset: 2,
+        },
+        "input:hover ~ &": {
+          filter: "brightness(1.2)",
+        },
+      },
+      checkedIcon: {
+        backgroundColor: color,
+        color: theme.palette.text.primary,
+        paddingTop: "5px",
+        "&:after": {
+          content: '"✔️"',
+        },
+        "input:hover ~ &": {},
+      },
+    });
+  })();
+
+const StyledRadio: React.FC<RadioProps & { radioButtonFill: string }> = (
+  props,
+) => {
+  const classes = useStyles(props.radioButtonFill);
+
+  return (
+    <Radio
+      className={classes.root}
+      disableRipple
+      color="default"
+      checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+      icon={<span className={classes.icon} />}
+      {...props}
+    />
+  );
+};
+
+function RadioButtonInputField() {
+  const [field, { error }] = useField({
+    name: "color",
+  });
+
+  return (
+    <FormControl error={Boolean(error)}>
+      <FormLabel htmlFor={field.name}>
+        <Typography variant="h6" align="center" color="textSecondary">
+          Color
+        </Typography>
+      </FormLabel>
+      <RadioGroup row defaultValue="#ff4d6e" {...field}>
+        <FormControlLabel
+          value="#ff4d6e"
+          control={<StyledRadio radioButtonFill="#ff4d6e" />}
+          label=""
+        />
+        <FormControlLabel
+          value="#2ef8f8"
+          control={<StyledRadio radioButtonFill="#2ef8f8" />}
+          label=""
+        />
+        <FormControlLabel
+          value="#e74af9"
+          control={<StyledRadio radioButtonFill="#e74af9" />}
+          label=""
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+export default RadioButtonInputField;
