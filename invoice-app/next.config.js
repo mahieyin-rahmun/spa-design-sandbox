af2d0b2dfdc24351078a9931a3badd1a3c96e27e
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2021-07-02 08:09:15 +0600

    chore(config): disable strict mode to prevent MUI from breaking

--- invoice-app/next.config.js
+++ invoice-app/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false, // causes issues with createMuiTheme
   webpack: function (config, options) {
     config.experiments = { topLevelAwait: true };
     return config;
